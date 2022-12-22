e19f3373c53746a577796f8a7a9fc76f6e86c1ad
esakkirajmari <esakkirajmari@gmail.com>
2022-12-22 10:07:40 +0530

    index.html

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let phone={
+    name:"",
+    network:"",
+    price:"",
+}
+console.log(phone);
+phone.name=prompt(("enter the name"));
+phone.network=prompt(("enter the network"));
+phone.price=prompt(("enter the price"));
+console.log(phone);
